940bd60953b3b25ef18fbf45da7cb4c7ff90b0ab
AlexRoque14 <171048@ids.upchiapas.edu.mx>
2020-01-23 19:19:32 -0600

    Settings

--- ClienteServidorDJ/settings.py
+++ ClienteServidorDJ/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')sp0nsig)!h8q**49*cwn=nabktb9rvk@t5-hgda4ylzo7%)qg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
